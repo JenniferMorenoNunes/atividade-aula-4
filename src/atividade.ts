91ce2cbd58cd989ee34d0a39c73dd0fa2b8f5abd
JenniferMorenoNunes <jenninunesmoreno@gmail.com>
2026-02-26 22:27:13 -0300

    removendo \n

--- src/atividade.ts
+++ src/atividade.ts
@@ -9,19 +9,19 @@ let op: number;
 do{ 
     console.log("MENU: ");
     console.log("1 - adicionar item; \n 2- listar itens; \n 3- remover itens; \n 0- sair: ");
-op = Number(prompt("escolha \n"));
+op = Number(prompt("escolha "));
 
     switch (op){
 
         case 1:
-            item = prompt("Qual item adicionar: \n");
+            item = prompt("Qual item adicionar: ");
             cadastrado.push(item);
             break;
         case 2: 
             console.log(cadastrado);
             break;
         case 3:
-            item = prompt("Qual item deseja remover: \n");
+            item = prompt("Qual item deseja remover: ");
             cadastrado.splice(cadastrado.indexOf(item), 1);
             break;
         default:
